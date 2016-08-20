53aea2bffff01a42ad574358e095763f3019ab85
re7410 <wind305503@gmail.com>
2016-08-20 22:25:35 +0800

    123

--- src/org/iii/www_abc/Aug20_1.java
+++ src/org/iii/www_abc/Aug20_1.java
@@ -3,8 +3,12 @@ package org.iii.www_abc;
 public class Aug20_1 {
 
 	public static void main(String[] args) {
-		System.out.println("ABCDE");
-		System.out.println("12345");
+		System.out.println("新增java project時,location選在"
+				+ "GitHub裡那個repository,ue,接著新增package,class,"
+				+ "完成程式撰寫,需要上傳至GitHub時,在project案右鍵,"
+				+ "點team,commit,把Aug20_1.java-src/org/iii/www_abc那串拉去Staged Changes放,"
+				+ "然後按commit and push上傳");
+
 
 	}
 
