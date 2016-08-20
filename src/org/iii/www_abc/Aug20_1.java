1656773f17b41b6125b188d101c2f222eeb79ec3
re7410 <wind305503@gmail.com>
2016-08-20 22:28:12 +0800

    123

--- src/org/iii/www_abc/Aug20_1.java
+++ src/org/iii/www_abc/Aug20_1.java
@@ -7,7 +7,7 @@ public class Aug20_1 {
 				+ "GitHub裡那個repository,ue,接著新增package,class,"
 				+ "完成程式撰寫,需要上傳至GitHub時,在project案右鍵,"
 				+ "點team,commit,把Aug20_1.java-src/org/iii/www_abc那串拉去Staged Changes放,"
-				+ "然後按commit and push上傳");
+				+ "然後按commit and push上傳,然後輸入GitHub帳號密碼完成即可,若沒有commit的話,是無法即時更新在GitHub的");
 
 
 	}
